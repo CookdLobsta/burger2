98a24c4524153614997f5cb0e1b8cfd1ca7eca56
Connor Wilson <connormwilson@gmail.com>
2019-01-26 04:09:38 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,41 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+if(process.env.JAWSDB_URL){
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+}else{
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
+
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+};
+// Export connection for our ORM to use.
+module.exports = connection;
